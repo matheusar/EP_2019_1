e140ff9395093808550af792cac62d32f0bb567b
Leonardo Alvarez <leonardosa2@al.insper.edu.br>
2019-04-22 22:06:02 -0300

    Adicionei um inventario e a batalha com o monstro

--- ep1.py
+++ ep1.py
@@ -4,6 +4,8 @@
 # - aluno A: Matheus Amaral Ricardo, matheusar@al.insper.edu.br
 # - aluno B: Leonardo Alvarez, leonardosa2@al.insper.edu.br
 
+
+
 def carregar_cenarios():
     cenarios = {
         "inicio": {
@@ -20,7 +22,7 @@ def carregar_cenarios():
             "opcoes": {
                 "inicio": "Tomar o elevador para o saguao de entrada",
                 "professor": "Falar com o professor",
-                "explorar": "Ir até a luz"
+                "investigar": "Ir até a luz"
                 
             }
         },
@@ -28,24 +30,60 @@ def carregar_cenarios():
             "titulo": "O monstro do Python",
             "descricao": "Voce foi pedir para o professor adiar o EP. "
                          "O professor revelou que é um monstro disfarçado "
-                         "e devorou sua alma.",
+                         "e quer devorar sua alma.",
             "opcoes": {}
         },
         "biblioteca": {
             "titulo": "Caverna da tranquilidade",
             "descricao": "Voce esta na biblioteca",
             "opcoes": {
-                "inicio": "Voltar para o saguao de entrada"
+                "inicio": "Voltar para o saguao de entrada",
+                "explorar": "Explorar o ambiente"
             }
         },
-        "explorar": {
+        "investigar": {
             "titulo": "Shazam",
             "descricao": "Voce achou o a maquina de teleporte!",
             "opcoes": {
-                    "inicio": "Teleportar para o saguao de entrada",
-                    "andar professor": "Teleportar para o andar do professor",
-                    "professor": "Teleportar para a sala do professor",
-                    "biblioteca": "Teleportar para a biblioteca"
+                    "inicio": "teleportar para o saguao de entrada",
+                    "andar professor": "teleportar para o andar do professor",
+                    "professor": "teleportar para a sala do professor",
+                    "biblioteca": "teleportar para a biblioteca"
+            }
+        },
+        "explorar": {
+            "titulo": "Uau",
+            "descricao": "Voce achou uma armadura!",
+            "opcoes": {
+                    "inicio": "Voltar para o saguao de entrada"
+            }
+        },
+        "fuga": {
+            "titulo": "O monstro do Python",
+            "descricao": "Voce foi pedir para o professor adiar o EP. "
+                         "O professor revelou que é um monstro disfarçado "
+                         "e quer devorar sua alma."
+                         "Por sorte, sua armadura te salvou do primeiro golpe do monstro,"
+                         "mas nao te salvará do segundo!",
+            "opcoes": {
+                    "fugir": "Fugir da sala imediatamente!",
+                    "ficar": "Aceitar o fim"
+            }
+        },
+        "fugir": {
+            "titulo": "Parabens",
+            "descricao": "Voce vonseguiu escapar com vida!",
+            "opcoes": {
+                    "gg": "FIM DE JOGO"
+                    
+            }
+        },
+        "ficar": {
+            "titulo": "Que pena!",
+            "descricao": "Voce morreu.",
+            "opcoes": {
+                    "gg": "FIM DE JOGO"
+                    
             }
         }
     }
@@ -54,6 +92,7 @@ def carregar_cenarios():
 
 
 def main():
+    armadura = 0
     print("Na hora do sufoco!")
     print("------------------")
     print()
@@ -84,13 +123,18 @@ def main():
         else:
             print("Qual seu próximo movimento?")
             for x, y in opcoes.items():
-                if nome_cenario_atual != "explorar":
+                if nome_cenario_atual != "investigar":
                     print("{0}: {1}".format(x,y))
                 # Imprime as opções de escolha para o jogador
                 
             escolha = input("O que fará a seguir? ")
             if escolha in opcoes:
+                
                 nome_cenario_atual = escolha
+                if escolha == "explorar":
+                    armadura += 1
+                elif escolha == "professor" and armadura > 0:
+                    nome_cenario_atual = "fuga"
             else:
                 print("Sua indecisão foi sua ruína!")
                 game_over = True
